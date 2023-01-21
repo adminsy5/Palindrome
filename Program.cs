4dc837a36e0a3f9002b2c5e4cb231daf7732f612
adminsy5 <piyush@cbigujarat.onmicrosoft.com>
2023-01-21 14:53:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿//Palindrome Programme Vb.net
+namespace Palindronme
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            int num, temp, revNum = 0, rem;
+            Console.WriteLine("Enter the number :");
+
+            num = Convert.ToInt32(Console.ReadLine());
+            temp = num;
+
+            while(num>0)
+            {
+                rem = num % 10;
+                revNum = revNum * 10 + rem;
+                num = num / 10;
+            }
+
+            if (revNum == temp)
+            {
+
+                Console.WriteLine("This is palindrome !");
+            }
+
+            else
+            {
+                Console.WriteLine("This is Not palindrome !");
+            }
+            Console.ReadLine();
+        }
+    }
+}
